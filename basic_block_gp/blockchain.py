0c92dd769c39a5fe447e7f3e2f76d71244f2d28c
Stefan <steaclem@gmail.com>
2019-08-07 18:18:17 -0700

    Added Current Chain I was missing

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -191,6 +191,8 @@ def new_transaction():
 def full_chain():
     response = {
         # TODO: Return the chain and its current length
+        'currentChain': blockchain.chain,
+        'length': len(blockchain.chain)
     }
     return jsonify(response), 200
 
